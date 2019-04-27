b8ef1906b69d8c106a9f14c8cd5a7b8b06cc5d02
kembo <lupin@kembo.org>
2019-04-27 19:08:37 +0900

    加算を実装

--- test_mynum.rb
+++ test_mynum.rb
@@ -14,4 +14,15 @@ class TestNaturalNumber < Test::Unit::TestCase
     assert_equal(@@Zero.succ, @@Zero.succ)
     assert_equal(@@Zero, @@Zero.succ.pred)
   end
+
+  @@nums = (1..4).inject({0 => @@Zero}){|h, i| h[i] = h[i-1].succ; h }
+  @@nums[100] = (5..100).inject(@@nums[4]){|n, i| n.succ }
+  data('1 + 0 = 1' => [@@nums[1], @@nums[0], @@nums[1]],
+       '0 + 1 = 1' => [@@nums[0], @@nums[1], @@nums[1]],
+       '1 + 2 = 3' => [@@nums[1], @@nums[2], @@nums[3]],
+       '100 + 2 = 102' => [@@nums[100], @@nums[2], @@nums[100].succ.succ],
+       '2 + 100 = 102' => [@@nums[2], @@nums[100], @@nums[100].succ.succ] )
+  def test_add(data)
+    assert_equal(data[2], data[0] + data[1])
+  end
 end
