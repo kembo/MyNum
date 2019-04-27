81f57f24d9331d763f30daf95aebba3accb4b56c
kembo <lupin@kembo.org>
2019-04-27 17:40:47 +0900

    Zero を定義

--- mynum.rb
+++ mynum.rb
@@ -8,6 +8,18 @@ module MyNum
 
   # 自然数のクラス
   class NaturalNumber < Numeric
+    # @!attribute [r] pred
+    #   @return [NaturalNumber, nil] その数の前者（0 の場合は nil）
+    attr_reader :pred
+
+    # @private
+    def initialize(pred: nil)
+      @pred = pred
+    end
+
+    @@Zero = new
+    # @return [NaturalNumber]
+    def self.Zero; @@Zero end
   end
 
   # 整数のクラス
--- test_mynum.rb
+++ test_mynum.rb
@@ -2,7 +2,9 @@ require 'test/unit'
 require_relative 'mynum'
 
 class TestMyNaturalNaumber < Test::Unit::TestCase
-  include MyNum
-
-  def test_cant_new; assert_raise { NaturalNumber.new } end
+  def test_cant_new; assert_raise { MyNum::NaturalNumber.new } end
+  def test_zero
+    assert_kind_of(MyNum::NaturalNumber, MyNum::NaturalNumber.Zero)
+    assert_nil(MyNum::NaturalNumber.Zero.pred)
+  end
 end
