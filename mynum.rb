1fba63b4504d3cc2f55360c327243dbc125a7dbf
kembo <lupin@kembo.org>
2019-04-29 20:13:15 +0900

    整数を定義

--- mynum.rb
+++ mynum.rb
@@ -7,28 +7,31 @@ module MyNum
   end
 
   # 自然数
+  # @!attribute [r] pred
+  #   @return [MyNum::NaturalNumber, nil] その数の前者（0 の場合は nil）
   class NaturalNumber < Numeric
+    attr_reader :pred
     class << self
       alias_method :_new, :new
       public :_new
     end
-    @@Zero = new
-    # @return [MyNum::NaturalNumber]
-    def self.Zero; @@Zero end
 
     # @private
     def initialize(pred: nil)
       @pred = pred
       @succ = nil
     end
-    # @!attribute [r] pred
-    #   @return [MyNum::NaturalNumber, nil] その数の前者（0 の場合は nil）
-    attr_reader :pred
     # @return [MyNum::NaturalNumber] その数の後者
     def succ
       @succ = NaturalNumber._new(pred: self) if @succ.nil?
       @succ
     end
+    @@Zero = new
+    # @return [MyNum::NaturalNumber]
+    def self.Zero; @@Zero end
+    @@One = @@Zero.succ
+    # @return [MyNum::NaturalNumber]
+    def self.One; @@One end
 
     DECA = (1..10).inject(@@Zero){|n,_| n.succ }
     # 文字列への変換
@@ -135,12 +138,48 @@ module MyNum
     # @return [Array<MyNum::NaturalNumber>] 余り
     def %(other); divmod(other).last end
   end
+  N = NaturalNumber
 
   # 整数のクラス
+  # @!attribute [r] minuend
+  #   @return [MyNum::NaturalNumber] 引かれる数
+  # @!attribute [r] subtrahend
+  #   @return [MyNum::NaturalNumber] 引く数
   class Integer < Numeric
+    attr_reader :minuend, :subtrahend
+    # @type [{Symbol=>{MyNum::NaturalNumber=>MyNum::Integer}}]
+    @@instances = {pos: {}, neg: {}}
+    class << self
+      # 整数のインスタンスを返す
+      # @param [MyNum::NaturalNumber] minuend 引かれる数
+      # @param [MyNum::NaturalNumber] subtrahend 引く数
+      # @return [MyNum::Integer]
+      def [](m, s)
+        unless m.kind_of?(NaturalNumber) and s.kind_of?(NaturalNumber)
+          raise TypeError.new("(m: #{m.class}, s: #{s.class})")
+        end
+        if s.pred.nil?
+          @@instances[:pos][m] = new(m,s) unless @@instances[:pos].has_key?(m)
+          return @@instances[:pos][m]
+        elsif m.pred.nil?
+          @@instances[:neg][s] = new(m,s) unless @@instances[:neg].has_key?(s)
+          return @@instances[:neg][s]
+        end
+        self.[](m.pred, s.pred)
+      end
+    end
+    # @private
+    def initialize(m, s)
+      @minuend = m
+      @subtrahend = s
+    end
+    @@Zero = self[N.Zero, N.Zero]
+    # @return [MyNum::Integer]
+    def self.Zero; @@Zero end
+    @@One  = self[N.One, N.Zero]
+    # @return [MyNum::Integer]
+    def self.One; @@One end
   end
 
-  # @!group エイリアス
-  N = NaturalNumber
   Z = Integer
 end
